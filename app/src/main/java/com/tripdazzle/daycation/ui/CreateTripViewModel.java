c65faaa159c1cb3de765bdb9a27d67544ce5d570
MARAUDERSMAP\gassa <lgassjsg@gmail.com>
2020-06-03 14:00:16 -0700

    Create view model for createTripFragment. Add text fields to layout.

--- app/src/main/java/com/tripdazzle/daycation/ui/CreateTripViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.tripdazzle.daycation.ui;
-
-import androidx.lifecycle.ViewModel;
-
-public class CreateTripViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/tripdazzle/daycation/ui/createtrip/CreateTripFragment.java
+++ app/src/main/java/com/tripdazzle/daycation/ui/createtrip/CreateTripFragment.java
@@ -1,16 +1,19 @@
-package com.tripdazzle.daycation.ui;
+package com.tripdazzle.daycation.ui.createtrip;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.tripdazzle.daycation.R;
+import com.tripdazzle.daycation.databinding.FragmentCreateTripBinding;
 
 public class CreateTripFragment extends Fragment {
 
@@ -23,13 +26,26 @@ public class CreateTripFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_create_trip, container, false);
+        mViewModel = ViewModelProviders.of(this).get(CreateTripViewModel.class);
+
+        FragmentCreateTripBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_create_trip, container, false);
+        binding.setViewModel(mViewModel);
+        binding.setLifecycleOwner(this);
+        View view = binding.getRoot();
+
+        view.findViewById(R.id.createTripSelectImageButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("CreateTripFragment", "select trip image clicked");
+            }
+        });
+
+        return view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(CreateTripViewModel.class);
     }
 
 }
